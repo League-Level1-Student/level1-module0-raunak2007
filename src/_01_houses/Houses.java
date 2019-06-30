387cfad0d140ed0dd9b10f03d31fc0edb707a533
League Student <league@wtsim17.attlocal.net>
2019-06-30 12:58:14 -0700

    module 0
    :smile:

---
+++ src/_01_houses/Houses.java
@@ -0,0 +1,13 @@
+package _01_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+	public static void main(String[] args) {
+		Robot rob=new Robot();
+		rob.setX(25);
+		rob.setY(300);
+		rob.penDown();
+		rob.move(100);
+	}
+}
